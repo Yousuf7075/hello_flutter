bc330c44772f9bd80149ca21b47c5ca2eac23a31
Aaraj Yousuf <aarajyousufdiu@gmail.com>
2020-11-07 11:33:19 +0600

    stack done

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,38 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Home()
-));
+void main() => runApp(MaterialApp(home: Home()));
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello Flutter"),
-        centerTitle: true,
-        backgroundColor: Colors.lightGreen,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child:
-              Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                height: 100,
-                width: 200,
-                decoration: BoxDecoration(
-                    color: Colors.lightBlueAccent,
-                    borderRadius: BorderRadius.circular(10)
-                ),
-                child: Text(
-                  "Container-1",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600
-                  ),
-                ),
-              ),
+        appBar: AppBar(
+          title: Text("Hello Flutter"),
+          centerTitle: true,
+          backgroundColor: Colors.lightGreen,
+        ),
+        body: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Material(color: Colors.yellowAccent),
+            Positioned(
+              top: 20,
+              left: 20,
+              child: Icon(Icons.star, size: 50),
             ),
-
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-              height: 100,
-              width: 200,
-              decoration: BoxDecoration(
-                  color: Colors.deepPurple,
-                  borderRadius: BorderRadius.circular(10)
-              ),
-              child: Text(
-                "Container-2",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600
+            Positioned(
+              top: 340,
+              left: 250,
+              child: IconButton(
+                onPressed: (){},
+                icon: Icon(
+                  Icons.call,
+                  size: 30,
                 ),
-              ),
+              )
             ),
-            Card()
           ],
-      )
-    );
+        ));
   }
 }
-
-class Card extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-      height: 100,
-      width: 200,
-      decoration: BoxDecoration(
-          color: Colors.pinkAccent,
-          borderRadius: BorderRadius.circular(10)
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Icon(
-            Icons.directions_bike_outlined,
-            color: Colors.white,
-            size: 28.0,
-          ),
-          Text(
-            'Go Forward',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.white
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-
