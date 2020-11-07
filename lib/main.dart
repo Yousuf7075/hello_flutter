4c0fcbcd45b53b00ca0e29f43f05f82cbd19401e
Aaraj Yousuf <aarajyousufdiu@gmail.com>
2020-11-07 11:12:05 +0600

    practice row column

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -12,22 +13,87 @@ class Home extends StatelessWidget{
       appBar: AppBar(
         title: Text("Hello Flutter"),
         centerTitle: true,
-        backgroundColor: Colors.orange,
+        backgroundColor: Colors.lightGreen,
       ),
-      body: Center(
-        child: Text(
-          "hello man!",
-          style: TextStyle(
-              fontSize: 20,
-              color: Colors.orange,
-              fontFamily: "Trispace"
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child:
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                height: 100,
+                width: 200,
+                decoration: BoxDecoration(
+                    color: Colors.lightBlueAccent,
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                child: Text(
+                  "Container-1",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600
+                  ),
+                ),
+              ),
+            ),
+
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+              height: 100,
+              width: 200,
+              decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(10)
+              ),
+              child: Text(
+                "Container-2",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600
+                ),
+              ),
+            ),
+            Card()
+          ],
+      )
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+      height: 100,
+      width: 200,
+      decoration: BoxDecoration(
+          color: Colors.pinkAccent,
+          borderRadius: BorderRadius.circular(10)
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            Icons.directions_bike_outlined,
+            color: Colors.white,
+            size: 28.0,
           ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Text("click"),
-        backgroundColor: Colors.orange,
-        onPressed: () {  },
+          Text(
+            'Go Forward',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.white
+            ),
+          )
+        ],
       ),
     );
   }
